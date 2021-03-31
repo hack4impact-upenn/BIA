3c8d70cd65f83494ee9dc210595be4933b685488
Janice Kim <janicekim60@gmail.com>
2021-03-31 14:39:40 -0400

    Added styling to map

--- src/client/src/components/Map.tsx
+++ src/client/src/components/Map.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import '../map.css';
+import '../mapbox-gl.css';
 
 mapboxgl.accessToken =
   'pk.eyJ1Ijoiams2MDYwNjA2MCIsImEiOiJja2J4Nm4yd3kwampvMnJwZ2l6a2hrY3RjIn0.7WBReAD5vV1__FmyUaPMbA';
@@ -13,36 +14,38 @@ const Map = () => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       // styles: https://docs.mapbox.com/api/maps/#styles
-      style: 'mapbox://styles/mapbox/dark-v10',
+      style: 'mapbox://styles/jk60606060/ckmxoum3w11cf17ld76oy7cgb',
       center: [-98.5795, 39.8283],
       zoom: 4.25,
     });
 
     map.on('load', function () {
-      // custom marker imag
-      map.loadImage(
-        'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-        function (error, image) {
-          if (error) throw error;
-          map.addImage('custom-marker', image);
-          map.addSource('earthquakes', {
-            type: 'geojson',
-            // random earthquake geojson file with points yay
-            data:
-              'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
-          });
+      // custom marker image
+      map.loadImage(process.env.PUBLIC_URL + '/img/marker.png', function (
+        error,
+        image
+      ) {
+        if (error) throw error;
+        map.addImage(process.env.PUBLIC_URL + '/img/marker.png', image);
+        map.addSource('earthquakes', {
+          type: 'geojson',
+          // random earthquake geojson file with points yay
+          data:
+            'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+        });
 
-          // Marker Layer
-          map.addLayer({
-            id: 'earthquakes',
-            type: 'symbol',
-            source: 'earthquakes',
-            layout: {
-              'icon-image': 'custom-marker',
-            },
-          });
-        }
-      );
+        map.addControl(new mapboxgl.NavigationControl());
+
+        // Marker Layer
+        map.addLayer({
+          id: 'earthquakes',
+          type: 'symbol',
+          source: 'earthquakes',
+          layout: {
+            'icon-image': process.env.PUBLIC_URL + '/img/marker.png',
+          },
+        });
+      });
     });
 
     map.on('click', 'earthquakes', function (e) {
