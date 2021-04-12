09ff02d4b2e8968a8d2b7cda1425f34731329f84
alnasir7 <alnasirvx@gmail.com>
2021-04-12 11:53:19 -0400

    cypress

--- src/client/src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../App';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-test('renders', () => {
-  render(<App />);
-});
