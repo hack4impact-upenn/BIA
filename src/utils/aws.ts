2e0631c69586777fbe66c59834b2975d92b41a2b
alnasir7 <alnasirvx@gmail.com>
2021-04-24 03:15:30 -0400

    fix upload

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -1,35 +1,55 @@
 const AWS = require('aws-sdk');
+const {
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_BUCKET_NAME,
+} = process.env;
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECTRET_ACCESS_KEY,
 });
 
-const awsUpload = (key: string, body: Buffer) => {
+const awsUpload = (key: string, body: Buffer, type: string, mime: string) => {
+  AWS.config.update({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  });
+  const s3Bucket = new AWS.S3({
+    params: { Bucket: process.env.AWS_BUCKET_NAME },
+  });
   const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
     Key: key,
     Body: body,
+    ACL: 'public-read',
+    ContentEncoding: 'base64',
+    ContentType: `${type}\${mime}`,
   };
-  s3.upload(params, (err: any, data: any) => {
+  s3Bucket.putObject(params, function (err: any, data: any) {
     if (err) {
-      return { success: false, data: err };
+      console.log(err);
+      console.log('Error uploading data: ', data);
+    } else {
+      console.log('successfully uploaded the image!');
     }
-    return { success: true, data: data };
   });
 };
 
-const awsGet = (key: string) => {
+const awsGet = async (key: string) => {
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: key,
   };
-  s3.getObject(params, (err: any, data: any) => {
+
+  var docs = await s3.getObject(params, (err: any, data: any) => {
     if (err) {
       return { success: false, data: err };
     }
-    return { success: true, data: data };
+    //console.log(data)
+    return data;
   });
+  //console.log(docs)
+  //return docs
 };
 
 module.exports = { awsUpload, awsGet };
