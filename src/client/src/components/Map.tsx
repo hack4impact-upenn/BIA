3cd5a22b332279fd73354237bf0b8b45f9fe2a79
alnasir7 <alnasirvx@gmail.com>
2021-06-07 01:23:26 -0400

    someprogress on alberUsa transformation

--- src/client/src/components/Map.tsx
+++ src/client/src/components/Map.tsx
@@ -2,13 +2,32 @@ import React, { useRef, useEffect } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '../map.css';
+import projections from 'dirty-reprojectors';
+
+const options = {
+  forward: 'albersUsa',
+  reverse: 'mercator',
+  projections: { alberUsa: 'alberUsa', mercator: 'mercator' },
+};
+console.log(projections(options, [35, -77]));
 
 //mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 mapboxgl.accessToken =
   'pk.eyJ1Ijoiams2MDYwNjA2MCIsImEiOiJja2J4Nm4yd3kwampvMnJwZ2l6a2hrY3RjIn0.7WBReAD5vV1__FmyUaPMbA';
 
-const Map = (data) => {
+const Map = (props) => {
+  const { data } = props;
+  const coords = data.map((org) => {
+    return { id: org.organizationName, coordinates: [org.lat, org.long] };
+  });
+  const source = {
+    type: 'canvas',
+    canvas: 'idOfMyHTMLCanvas',
+    animate: true,
+    coordinates: coords,
+  };
+
   const mapContainerRef = useRef(null);
 
   // initialize map
@@ -30,22 +49,20 @@ const Map = (data) => {
         map.addImage(process.env.PUBLIC_URL + '/img/marker.png', image);
         map.addSource('earthquakes', {
           type: 'geojson',
-          // random earthquake geojson file with points yay
+          //random earthquake geojson file with points yay
           data:
             'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
         });
 
         map.addControl(new mapboxgl.NavigationControl());
 
-        // Marker Layer
-        map.addLayer({
-          id: 'earthquakes',
-          type: 'symbol',
-          source: 'earthquakes',
-          layout: {
-            'icon-image': process.env.PUBLIC_URL + '/img/marker.png',
-          },
+        data.forEach((org) => {
+          var coordinates = [org.long + 96, org.lat - 35];
+
+          var marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
         });
+
+        // Marker Layer
       });
     });
 
