49c1bf823812cb168c16ff3bb5cd33f3c532b797
alnasir7 <alnasirvx@gmail.com>
2021-06-24 20:00:06 -0400

    geolocator spreading

--- src/utils/geocoder.ts
+++ src/utils/geocoder.ts
@@ -23,15 +23,31 @@ class Locator {
   }
 
   async getLocation(location: string) {
+    const stringLocation = location;
     return axios
       .get(
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?types=place&access_token=pk.eyJ1Ijoiams2MDYwNjA2MCIsImEiOiJja2J4Nm4yd3kwampvMnJwZ2l6a2hrY3RjIn0.7WBReAD5vV1__FmyUaPMbA`
       )
       .then((resp) => {
+        var rad = 5000;
+        if (
+          stringLocation.toLowerCase().includes('washington') ||
+          stringLocation.toLowerCase().includes('atlanta') ||
+          stringLocation.toLowerCase().includes('austin')
+        ) {
+          rad = 18000;
+        }
+        if (
+          stringLocation.toLowerCase().includes('new york') ||
+          stringLocation.toLowerCase().includes('los angeles')
+        ) {
+          rad = 12000;
+        }
+
         const location = { lat: 0, lng: 0 };
         location.lng = resp.data.features[0].center[0];
         location.lat = resp.data.features[0].center[1];
-        const rad = 5000;
+
         return this.generateRandomPoint(location, rad);
       });
   }
